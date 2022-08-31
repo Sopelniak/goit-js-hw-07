d3c1dea47d6536a360c92cbd578e37dac4f7c97d
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-08-31 17:35:06 +0300

    створив розмітку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// import * as basicLightbox from "basiclightbox";
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.reduce((acc, item) => {
+  return (
+    acc +
+    `<img class="js-gallery-item" src="${item.original}" alt="${item.description}" />`
+  );
+}, "");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onClickImg);
+function onClickImg(e) {
+  if (!e.target.classList.contains("js-gallery-item")) {
+    return;
+  }
+  const urlImg = e.target.getAttribute("src");
+  console.log("target:", e.target);
+  console.log("url:", urlImg);
+}
