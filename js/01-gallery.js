1a5b49d5eac60544becd12f282aaa06bc3a4b4ee
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-01 01:50:27 +0300

    gallery 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,23 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import * as basicLightbox from "../node_modules/basiclightbox/src/scripts/main.js";
 
 // console.log(galleryItems);
-
-// import * as basicLightbox from "basiclightbox";
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `);
-
-// instance.show();
 
 const gallery = document.querySelector(".gallery");
 const markup = galleryItems.reduce((acc, item) => {
   return (
     acc +
-    `<img class="js-gallery-item" src="${item.original}" alt="${item.description}" />`
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
   );
 }, "");
 
@@ -27,6 +29,12 @@ function onClickImg(e) {
     return;
   }
   const urlImg = e.target.getAttribute("src");
-  console.log("target:", e.target);
-  console.log("url:", urlImg);
+
+  const instance = basicLightbox.create(`
+    <img src=${urlImg} width="800" height="600">
+`);
+
+  instance.show();
 }
+
+// e.preventDefault();
