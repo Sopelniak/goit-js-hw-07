cc5667ce2422a4aaeefba4aa721dd8e45d8952e9
Sopelniak <106702004+Sopelniak@users.noreply.github.com>
2022-09-05 22:04:53 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,10 +14,8 @@ const markup = galleryItems.reduce(
 gallery.innerHTML = markup;
 
 function createLightbox() {
-  const gallery = new SimpleLightbox(".gallery a");
+  const gallery = new SimpleLightbox(".gallery a", { captionDelay: 250 });
 
-  gallery.on("show.simplelightbox", function () {
-    // captionDelay = 250;
-  });
+  gallery.on("show.simplelightbox", function () {});
 }
 createLightbox();
